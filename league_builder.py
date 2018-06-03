54a1c28afbbf7cd3a6153acba74b76c47a371633
python-force <studio@pythonforce.com>
2018-06-03 17:35:39 -0400

    writing 1st team to a file

--- league_builder.py
+++ league_builder.py
@@ -18,6 +18,9 @@ TEAMS = 3
 team_one = []
 team_two = []
 team_three = []
+TEAM_ONE_NAME = "Sharks"
+TEAM_TWO_NAME = "Dragons"
+TEAM_THREE_NAME = "Raptors"
 
 # Function for CSV to JSON conversion
 def csv_to_json():
@@ -53,9 +56,13 @@ def create_teams_based_on_experience(experienced, inexperienced):
     team_three.extend(inexperienced[TEAMS*2:TEAMS*3])
 
 # Function for writing the teams into the file
-def list_of_teams():
+def list_of_teams(team_one, team_two, team_three):
     with open("teams.txt", "w") as file:
-        file.write("hello")
+        file.write(TEAM_ONE_NAME + "\n")
+        for player in team_one:
+            file.write(player["Name"] + ", ")
+            file.write(player["Soccer Experience"] + ", ")
+            file.write(player["Guardian Name(s)"] + "\n")
 
 # Converting data CSV to JSON
 csv_to_json()
@@ -67,7 +74,7 @@ experience_level()
 create_teams_based_on_experience(experienced, inexperienced)
 
 # Write list of teams to teams.txt file
-list_of_teams()
+list_of_teams(team_one, team_two, team_three)
 
 # Checking the results
 for i in range(0, len(team_one)):
