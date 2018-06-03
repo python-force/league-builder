fb884efc929478029b0e4fcb225e89f8b2578f96
python-force <studio@pythonforce.com>
2018-06-03 16:53:08 -0400

    function for CSV to JSON

--- league_builder.py
+++ league_builder.py
@@ -2,3 +2,23 @@
 import csv
 import json
 
+# Specify the file where to read the players data from
+# Converting data from CSV to JSON
+csv_filename = input("Specify the file that you wish to process: ")
+json_filename = csv_filename.split(".")[0] + ".json"
+
+# Function for CSV to JSON conversion
+def csv_to_json():
+    try:
+        with open(csv_filename, 'r') as csvfile:
+            soccer_reader = csv.DictReader(csvfile, delimiter=",")
+            rows = list(soccer_reader)
+            with open(json_filename, 'w') as soccerfile:
+                json.dump(rows, soccerfile)
+    except:
+        print("The file that you have specified has not been found, please try again.")
+
+
+
+# Converting data CSV to JSON
+csv_to_json()
