74807d9a49b164fd8009301d79e71b5fdef8ccca
python-force <studio@pythonforce.com>
2018-06-03 16:56:06 -0400

    function for dividing exp and inexp players

--- league_builder.py
+++ league_builder.py
@@ -2,10 +2,15 @@
 import csv
 import json
 
+# Specifying the Instance Variables / Defaults
 # Specify the file where to read the players data from
 # Converting data from CSV to JSON
 csv_filename = input("Specify the file that you wish to process: ")
 json_filename = csv_filename.split(".")[0] + ".json"
+
+# Experienced and Inexperienced groups / lists inits
+experienced = []
+inexperienced = []
 
 # Function for CSV to JSON conversion
 def csv_to_json():
@@ -18,7 +23,21 @@ def csv_to_json():
     except:
         print("The file that you have specified has not been found, please try again.")
 
-
+# Function for dividing experienced and inexperienced players
+def experience_level():
+    try:
+        with open(json_filename, 'r') as soccer:
+            players = json.load(soccer)
+            for player in players:
+                if player["Soccer Experience"] == "YES":
+                    experienced.append(player)
+                else:
+                    inexperienced.append(player)
+    except:
+        print("The file that you have specified has not been found, please try again.")
 
 # Converting data CSV to JSON
 csv_to_json()
+
+# Dividing experienced and inexperienced players
+experience_level()
